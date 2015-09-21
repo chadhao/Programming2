b17d19182021c8af15d5468e76b4945775761437
Hao Duan <haoduan@outlook.com>
2015-09-21 22:52:18 +1200

    add SimpleJet class

--- assignment2/Airline Reservation Application/src/AirlineReservationApplication.java
+++ assignment2/Airline Reservation Application/src/AirlineReservationApplication.java
@@ -4,8 +4,8 @@ public class AirlineReservationApplication
 
 	public static void main(String[] args)
 	{
-		SeatMap seats = new AirBusSeatMap();
-		System.out.println(seats.getLeft(seats.getSeat(5, 'D')).seatDescription());
+		SeatMap seats = new BoeingSeatMap();
+		System.out.println(seats);
 		
 	}
 
--- assignment2/Airline Reservation Application/src/SimpleJet.java
+++ assignment2/Airline Reservation Application/src/SimpleJet.java
@@ -16,8 +16,23 @@ public class SimpleJet extends Airline
 		}
 		else
 		{
-			
+			if (aFlight.getSeats().queryAvailableEconomySeat(SeatType.M) != null && aFlight.getSeats().queryAvailableEconomySeat(SeatType.M).getType() == SeatType.M)
+			{
+				if (aFlight.getSeats().getLeft(aFlight.getSeats().queryAvailableEconomySeat(SeatType.M)) != null)
+				{
+					aSeat = aFlight.getSeats().queryAvailableEconomySeat(SeatType.M);
+					aSeat.reserveSeat();
+					aFlight.getSeats().getLeft(aSeat).reserveSeat();
+				}
+				else if (aFlight.getSeats().getRight(aFlight.getSeats().queryAvailableEconomySeat(SeatType.M)) != null)
+				{
+					aSeat = aFlight.getSeats().queryAvailableEconomySeat(SeatType.M);
+					aSeat.reserveSeat();
+					aFlight.getSeats().getRight(aSeat).reserveSeat();
+				}
+			}
 		}
+		return aSeat;
 	}
 	
 	public Seat reserveEconomy(Flight aFlight, SeatType seatType)
