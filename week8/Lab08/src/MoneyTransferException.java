74f49d75763443b10124a6f1e081dddcda8a720e
Hao Duan <haoduan@outlook.com>
2015-09-22 13:45:02 +1200

    add class MoneyTransferException

--- week8/Lab08/src/MoneyTransferException.java
+++ week8/Lab08/src/MoneyTransferException.java
@@ -1,5 +1,25 @@
 
-public class MoneyTransferException
+public class MoneyTransferException extends IllegalArgumentException
 {
+	private long timeStamp;
+	
+	public MoneyTransferException()
+	{
+		super();
+	}
 
+	public MoneyTransferException(String message)
+	{
+		super(message);
+	}
+	
+	public long getTimeStamp()
+	{
+		return timeStamp;
+	}
+
+	public void setTimeStamp(long timeStamp)
+	{
+		this.timeStamp = timeStamp;
+	}
 }
