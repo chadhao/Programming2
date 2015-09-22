e6ad690268cada2d190618152683900a90885611
Hao Duan <haoduan@outlook.com>
2015-09-22 12:52:26 +1200

    Week8 lab ex02 & ex03

---
+++ week8/Lab08/src/Exercise2.java
@@ -0,0 +1,18 @@
+
+public class Exercise2
+{
+	public static void main(String args[])
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			try
+			{
+				throw new Exception("exception caught!");
+			}
+			catch (Exception e)
+			{
+				System.err.println((i+1) + ". " + e);
+			}
+		}
+	}
+}
---
+++ week8/Lab08/src/Exercise3.java
@@ -0,0 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercise3
+{
+	private static Scanner scanner;
+	
+	public static void product()
+	{
+		System.out.println("Enter number 1:");
+		int value1 = scanner.nextInt();
+		System.out.println("Enter number 2:");
+		int value2 = scanner.nextInt();
+		System.out.println("Product is: " + (value1*value2));
+	}
+	
+	public static void main(String args[])
+	{
+		scanner = new Scanner(System.in);
+		System.out.println("Welcome to the calculator.");
+		try
+		{
+			product();
+		}
+		catch (InputMismatchException ime)
+		{
+			System.err.println(ime);
+			scanner.next();
+		}
+	}
+}
