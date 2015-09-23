3e7bfd3499374bbb61e5ad4dbc122ebccf286f16
Hao Duan <haoduan@outlook.com>
2015-09-23 23:10:11 +1200

    add method randomFillSeats(SeatMap) to class AirlineReservationApplication

--- assignment2/Airline Reservation Application/src/AirlineReservationApplication.java
+++ assignment2/Airline Reservation Application/src/AirlineReservationApplication.java
@@ -1,12 +1,23 @@
+import java.util.Random;
 
 public class AirlineReservationApplication
 {
-
+	
 	public static void main(String[] args)
 	{
 		SeatMap seats = new AirBusSeatMap();
 		System.out.println(seats);
-		
+		randomFillSeats(seats);
+		System.out.println(seats);
 	}
-
+	
+	public static void randomFillSeats(SeatMap seats)
+	{
+		Random rand = new Random();
+		for (int i = 0; i < 20; i++)
+		{
+			
+			seats.getSeat(rand.nextInt(seats.getNumOfRows())+1, (char)(Character.valueOf('A')+rand.nextInt(seats.getNumOfColumns()))).reserveSeat();
+		}
+	}
 }
