8e4a343c572cdf90832bfe5cffc96d09d53050b7
Hao Duan <haoduan@outlook.com>
2015-10-05 14:34:59 +1300

    exercise Stack

--- week9/Lab09/src/StackTest.java
+++ week9/Lab09/src/StackTest.java
@@ -2,17 +2,45 @@ import java.util.*;
 
 public class StackTest
 {
-
+	private static Scanner keyboard = new Scanner(System.in);
+	
 	public static void main(String[] args)
 	{
 		Stack<Integer> stack = new Stack<Integer>();
 		
-		stack.push(1);
-		stack.push(2);
-		Integer i = stack.pop();
-		Integer j = stack.pop();
-		System.out.println(i);
-		System.out.println(j);
+		pushOntoStack(stack);
+		
+	}
+	
+	public static void pushOntoStack(Stack<Integer> stack)
+	{
+		while(true)
+		{
+			int aNum;
+			System.out.print("Enter an integer: ");
+			try
+			{
+				aNum = keyboard.nextInt();
+			}
+			catch (InputMismatchException ime)
+			{
+				if (keyboard.next().equals("quit"))
+				{
+					System.out.println("<finish input>");
+					break;
+				}
+				else
+				{
+					System.out.println("Invalid input! Try Again!");
+					continue;
+				}
+			}
+			stack.push(aNum);
+		}
+		while(!stack.empty())
+		{
+			System.out.print(stack.pop() + " ");
+		}
 	}
 
 }
