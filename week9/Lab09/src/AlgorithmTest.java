da4beab3e06ce5ee8143f12a38765bf341303de7
Hao Duan <haoduan@outlook.com>
2015-10-06 12:44:24 +1300

    exercise Algorithms on ArrayList

--- week9/Lab09/src/AlgorithmTest.java
+++ week9/Lab09/src/AlgorithmTest.java
@@ -3,13 +3,26 @@ import java.util.*;
 public class AlgorithmTest
 {
 	private static Scanner keyboard = new Scanner(System.in);
+	private static Random rand = new Random();
 	
 	public static void main(String[] args)
 	{
-		ArrayList<Integer> anArrayList = inputInteger(20);
+		ArrayList<Integer> anArrayList = inputInteger(15);
 		System.out.println(anArrayList);
 		System.out.println("The max number is " + getMax(anArrayList));
 		System.out.println("Number " + whichNumberNotRepeat(anArrayList) + " does not repeat.");
+		
+		anArrayList.clear();
+		for (int i = 0; i < 100; i++)
+		{
+			if (rand.nextInt(100) > 1)
+			{
+				anArrayList.add(i+1);
+			}
+		}
+		Collections.shuffle(anArrayList);
+		int numOfMissing = whichNumberIsMissing(anArrayList);
+		System.out.println(numOfMissing<2?numOfMissing==1?"1 number is missing":"nothing is missing":numOfMissing + " numbers are missing");
 	}
 	
 	public static ArrayList<Integer> inputInteger(int n)
@@ -42,11 +55,29 @@ public class AlgorithmTest
 		return Collections.max(anArrayList);
 	}
 	
-//	Don't quite understand the requirement of this question.
-//	public static Integer whichNumberIsMissing(ArrayList<Integer> anArrayList)
-//	{
-//		
-//	}
+	public static Integer whichNumberIsMissing(ArrayList<Integer> anArrayList)
+	{
+		int counter = 0;
+		int[] isEmpty = new int[100];
+		for (int i = 0; i < 100; i++) isEmpty[i] = 0;
+		Iterator<Integer> it = anArrayList.iterator();
+		while(it.hasNext())
+		{
+			int next = it.next();
+			if (next != 0)
+			{
+				isEmpty[next-1] = 1;
+			}
+		}
+		for (int a : isEmpty)
+		{
+			if (a == 0)
+			{
+				counter++;
+			}
+		}
+		return counter;
+	}
 
 	public static Integer whichNumberNotRepeat(ArrayList<Integer> anArrayList)
 	{
