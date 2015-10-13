ec6b24d6c6f86677999f8c501748399e02b414f3
Hao Duan <haoduan@outlook.com>
2015-10-13 13:44:32 +1300

    lab11 Binary Tree exercise2

--- week11/Lab11/src/BinaryTree.java
+++ week11/Lab11/src/BinaryTree.java
@@ -15,4 +15,27 @@ public class BinaryTree<T extends Comparable<T>>
 	{
 		return data.toString();
 	}
+	
+	public BinaryTree<T> insert(T element)
+	{
+		if (element.compareTo(this.data) == 0)
+		{
+			return this;
+		}
+		if (element.compareTo(this.data) < 0)
+		{
+			if (this.left == null)
+			{
+				this.left = new BinaryTree<T>(element);
+				return this.left;
+			}
+			return this.left.insert(element);
+		}
+		if (this.right == null)
+		{
+			this.right = new BinaryTree<T>(element);
+			return this.right;
+		}
+		return this.right.insert(element);
+	}
 }
---
+++ week11/Lab11/src/BinaryTreeTest.java
@@ -0,0 +1,13 @@
+
+public class BinaryTreeTest
+{
+
+	public static void main(String[] args)
+	{
+		BinaryTree<Integer> aTree = new BinaryTree<Integer>(4);
+		aTree.insert(3);
+		aTree.insert(6);
+		aTree.insert(2);
+	}
+
+}
