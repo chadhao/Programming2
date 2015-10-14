602f7ef0536071951c5eae26e4d492d5ff8334e5
Hao Duan <haoduan@outlook.com>
2015-10-14 16:29:12 +1300

    modify class ServiceException

--- assignment3/TheDigitalServiceProvisioningFramework/src/ServiceException.java
+++ assignment3/TheDigitalServiceProvisioningFramework/src/ServiceException.java
@@ -1,15 +1,16 @@
+import java.util.Date;
 
 public class ServiceException extends Exception
 {
 	private Account errAccount;
 	private String errDescription;
-	private long errTime;
+	private Date errTime;
 	
 	public ServiceException(Account errAccount, String errDescription)
 	{
 		this.errAccount = errAccount;
 		this.errDescription = errDescription;
-		this.errTime = System.currentTimeMillis();
+		this.errTime = new Date(System.currentTimeMillis());
 	}
-		
+	
 }
