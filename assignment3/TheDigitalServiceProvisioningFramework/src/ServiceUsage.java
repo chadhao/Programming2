c36d493fcf63aedf2965ca047ae6f5ca60957374
Hao Duan <haoduan@outlook.com>
2015-10-14 16:12:21 +1300

    modify class ServiceUsage

--- assignment3/TheDigitalServiceProvisioningFramework/src/ServiceUsage.java
+++ assignment3/TheDigitalServiceProvisioningFramework/src/ServiceUsage.java
@@ -1,5 +1,11 @@
 
-public abstract class ServiceUsage
+public abstract class ServiceUsage implements Comparable<ServiceUsage>
 {
+	private String serviceName;
 	
+	@Override
+	public int compareTo(ServiceUsage o)
+	{
+		return this.serviceName.compareTo(o.serviceName);
+	}
 }
