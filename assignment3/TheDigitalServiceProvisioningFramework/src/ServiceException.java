d983356490cb98a0eb3a915c6607b70eb8d2b32e
Hao Duan <haoduan@outlook.com>
2015-10-14 16:23:49 +1300

    add class ServiceException

--- assignment3/TheDigitalServiceProvisioningFramework/src/ServiceException.java
+++ assignment3/TheDigitalServiceProvisioningFramework/src/ServiceException.java
@@ -1,5 +1,15 @@
 
-public class ServiceException
+public class ServiceException extends Exception
 {
-
+	private Account errAccount;
+	private String errDescription;
+	private long errTime;
+	
+	public ServiceException(Account errAccount, String errDescription)
+	{
+		this.errAccount = errAccount;
+		this.errDescription = errDescription;
+		this.errTime = System.currentTimeMillis();
+	}
+		
 }
