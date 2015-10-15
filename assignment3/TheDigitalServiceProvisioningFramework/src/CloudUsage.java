740a56c55c6b16b65e39b74973667fa4c33c1353
Hao Duan <haoduan@outlook.com>
2015-10-15 17:08:56 +1300

    update class CloudUsage

--- assignment3/TheDigitalServiceProvisioningFramework/src/CloudUsage.java
+++ assignment3/TheDigitalServiceProvisioningFramework/src/CloudUsage.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
 
 public class CloudUsage extends ServiceUsage
 {
+	private static final ArrayList<Product> INIT_LIST = new ArrayList<>();
+	
+	static
+	{
+		INIT_LIST.add(new Product("Virtual Machines", 0.14));
+		INIT_LIST.add(new Product("Disk Storage Devices", 0.51));
+	}
+	
 	public CloudUsage()
 	{
-		super(null, null);
+		super("Cloud Service", INIT_LIST);
 	}
+	
+	
 }
