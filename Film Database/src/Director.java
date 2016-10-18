4aa51e38dcfff73ee322a37ab86dd2e2050860ea
Duan Hao <haoduan@outlook.com>
2016-10-18 18:05:41 +1300

    update @ 6:05PM 18/10/2016

--- Film Database/src/Director.java
+++ Film Database/src/Director.java
@@ -1,7 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
 /**
  *
  * @author Chad
  */
 public class Director {
+    private static ArrayList<String> directors;
+    
+    static {
+	Director.directors = new ArrayList<>();
+    }
+    
+    public static void init(ArrayList<String> directors) {
+	Director.directors = new ArrayList<>(new HashSet<>(directors));
+    }
+    
+    public static String getDirector(int index) {
+	return Director.directors.get(index);
+    }
+    
+    public static String[] getDirectors(int[] index) {
+	String[] film_directors = new String[index.length];
+	for (int i = 0; i < index.length; i++) {
+	    film_directors[i] = Director.directors.get(index[i]);
+	}
+	return film_directors;
+    }
+    
+    public static boolean contains(String name) {
+	Iterator<String> it = Director.directors.iterator();
+	while (it.hasNext()) {
+	    if (it.next().equalsIgnoreCase(name)) {
+		return true;
+	    }
+	}
+	return false;
+    }
     
 }
--- Film Database/src/Film.java
+++ Film Database/src/Film.java
@@ -1,7 +1,15 @@
+
+import java.util.ArrayList;
+
 /**
  *
  * @author Chad
  */
 public class Film {
-    
+    private String title;
+    private String year;
+    private Genre genre;
+    private Rating rating;
+    private int[] directors;
+    private int[] casts;
 }
