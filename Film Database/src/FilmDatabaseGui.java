fcd35aefaed73ee9828bd633c4d5178b0bf8b858
Duan Hao <haoduan@outlook.com>
2016-10-20 18:03:43 +1300

    update @ 6:03PM 20/10/2016

--- Film Database/src/FilmDatabaseGui.java
+++ Film Database/src/FilmDatabaseGui.java
@@ -4,6 +4,21 @@
  */
 public class FilmDatabaseGui {
     public static void main(String[] args) {
+	String[] directors = {"wen jiang"};
+	String[] casts = {"yun-fat chow", "wen jiang", "you ge"};
+	Genre[] genres = {Genre.ACTION, Genre.COMEDY, Genre.WESTERN};
+	FilmDatabase.directorsList.addName(directors);
+	FilmDatabase.castsList.addName(casts);
+	Film aFilm = new Film(
+		"Let the Bullets Fly",
+		"2010",
+		genres,
+		Rating.FOUR,
+		FilmDatabase.directorsList.getNameList(directors),
+		FilmDatabase.castsList.getNameList(casts)
+	);
+	FilmDatabase.films.add(aFilm);
 	
+	System.out.println(FilmDatabase.listAllFilms());
     }
 }
--- Film Database/src/Rating.java
+++ Film Database/src/Rating.java
@@ -3,19 +3,25 @@
  * @author Chad
  */
 public enum Rating {
-    ONE("★☆☆☆☆"),
-    TWO("★★☆☆☆"),
-    THREE("★★★☆☆"),
-    FOUR("★★★★☆"),
-    FIVE("★★★★★");
+    ONE("★☆☆☆☆", 1),
+    TWO("★★☆☆☆", 2),
+    THREE("★★★☆☆", 3),
+    FOUR("★★★★☆", 4),
+    FIVE("★★★★★", 5);
     
-    private String rating;
+    private String ratingStar;
+    private int rating;
     
-    private Rating(String rating) {
+    private Rating(String ratingStar, int rating) {
 	this.rating = rating;
+	this.ratingStar = ratingStar;
     }
     
-    public String getRating() {
+    public int getRating() {
 	return this.rating;
+    }
+    
+    public String getRatingStar() {
+	return this.ratingStar;
     }
 }
